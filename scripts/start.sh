e17550f26763af8535532d59be5f5f9effa46145
Nodiaque <324425+nodiaque@users.noreply.github.com>
2026-07-23 22:28:32 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-kill -n 9 $(pidof WindroseServer-Linux-Shipping)
-kill -n 9 $(pidof sh)
-kill -n 9 1
-sleep 20
 
 echo "---Ensuring UID: ${UID} matches user---"
 usermod -u ${UID} ${USER}
@@ -13,19 +9,19 @@ echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
 echo "---Taking ownership of data...---"
-chown -R root:${GID} /home/ue_user/scripts
-chmod -R 750 /home/ue_user/scripts
+chown -R root:${GID} /opt/scripts
+chmod -R 750 /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 
 echo "---Starting...---"
 term_handler() {
-	kill -n 9 $(pidof WindroseServer-Linux-Shipping)
+	kill -SIGINT $(pidof WindroseServer-Linux-Shipping)
 	tail --pid=$(pidof WindroseServer-Linux-Shipping) -f 2>/dev/null
 	exit 143;
 }
 
 trap 'kill ${!}; term_handler' SIGTERM
-su ${USER} -c "/home/ue_user/scripts/start-server.sh" &
+su ${USER} -c "/opt/scripts/start-server.sh" &
 killpid="$!"
 while true
 do
