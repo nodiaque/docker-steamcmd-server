03e0c8339b9a422e4496fcb07349919885b7470c
Nodiaque <324425+nodiaque@users.noreply.github.com>
2026-07-23 22:43:35 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "---Ensuring UID: ${UID} matches user---"
 usermod -u ${UID} ${USER}
 echo "---Ensuring GID: ${GID} matches user---"
@@ -7,6 +6,18 @@ groupmod -g ${GID} ${USER} > /dev/null 2>&1 ||:
 usermod -g ${GID} ${USER}
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
+
+echo "---Checking for optional scripts---"
+cp -f /opt/custom/user.sh /opt/scripts/start-user.sh > /dev/null 2>&1 ||:
+cp -f /opt/scripts/user.sh /opt/scripts/start-user.sh > /dev/null 2>&1 ||:
+
+if [ -f /opt/scripts/start-user.sh ]; then
+  echo "---Found optional script, executing---"
+  chmod -f +x /opt/scripts/start-user.sh ||:
+  /opt/scripts/start-user.sh || echo "---Optional Script has thrown an Error---"
+else
+  echo "---No optional script found, continuing---"
+fi
 
 echo "---Taking ownership of data...---"
 chown -R root:${GID} /opt/scripts
