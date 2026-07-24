3a151d4d52c74c8dc7c3ad1880f1c904c89f5e2f
Nodiaque <324425+nodiaque@users.noreply.github.com>
2026-07-23 20:49:29 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,21 +7,9 @@ usermod -g ${GID} ${USER}
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
-echo "---Checking for optional scripts---"
-cp -f /opt/custom/user.sh /opt/scripts/start-user.sh > /dev/null 2>&1 ||:
-cp -f /opt/scripts/user.sh /opt/scripts/start-user.sh > /dev/null 2>&1 ||:
-
-if [ -f /opt/scripts/start-user.sh ]; then
-  echo "---Found optional script, executing---"
-  chmod -f +x /opt/scripts/start-user.sh ||:
-  /opt/scripts/start-user.sh || echo "---Optional Script has thrown an Error---"
-else
-  echo "---No optional script found, continuing---"
-fi
-
 echo "---Taking ownership of data...---"
-chown -R root:${GID} /opt/scripts
-chmod -R 750 /opt/scripts
+chown -R root:${GID} /home/ue_user/scripts
+chmod -R 750 /home/ue_user/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 
 echo "---Starting...---"
@@ -32,7 +20,7 @@ term_handler() {
 }
 
 trap 'kill ${!}; term_handler' SIGTERM
-su ${USER} -c "/opt/scripts/start-server.sh" &
+su ${USER} -c "/home/ue_user/scripts/start-server.sh" &
 killpid="$!"
 while true
 do
